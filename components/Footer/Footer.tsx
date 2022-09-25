a261325fd2cdd0a449ce2c186d66cdbf990f4e59
Hayden Rose <hayden4r4@gmail.com>
2022-09-25 14:25:44 -0700

    Mostly finished footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
+
 export default function Footer() {
-    return (
-        <div id="footer">
-            <p>This is a footer</p>
-        </div>
-    );
+	return (
+		<div id="footer">
+			<div id="footerContent">
+                <span id="footerMainText">
+                    <p id="footerName">Hayden Rose</p>
+                    <Link href="mailto:haydenrose@hayden-rose.com">
+                        <a id="footerEmail">haydenrose@hayden-rose.com</a>
+                    </Link>
+                    <Link href="https://github.com/hayden4r4/personal_website_frontend_nextjs/tree/master">
+                        <a id="footerPageSourceLink">View this page on GitHub ↗</a>
+                    </Link>
+                </span>
+				<Link href="https://github.com/hayden4r4/personal_website_frontend_nextjs/blob/master/LICENSE.md">
+					<a id="footerLicense">
+						Licensed Under the GPLv3 License
+					</a>
+				</Link>
+			</div>
+		</div>
+	);
 }
