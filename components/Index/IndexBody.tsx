577ff5e7f26207d73d6f8903486d550eb2d80a8e
Hayden Rose <hayden4r4@gmail.com>
2022-10-30 13:49:38 -0700

    Added more portfolio projects

--- components/Index/IndexBody.tsx
+++ components/Index/IndexBody.tsx
@@ -1,11 +1,16 @@
 import styles from "../../public/static/styles/utilities/_constants.module.scss";
-import textShadowGenerator from '../Utilities/TextEffects';
+import textShadowGenerator from "../Utilities/TextEffects";
 
 export default function IndexBody() {
 	return (
 		<div className="indexBody">
 			<div className="indexBodyWelcome">
-				<h1 id="indexBodyWelcomeText" style={{ textShadow: textShadowGenerator(0, .1, .0025, styles.marsPink) }}>
+				<h1
+					id="indexBodyWelcomeText"
+					style={{
+						textShadow: textShadowGenerator(0, 0.1, 0.0025, styles.marsPink),
+					}}
+				>
 					Please hire me
 				</h1>
 			</div>
--- components/Portfolio/PortfolioBody.tsx
+++ components/Portfolio/PortfolioBody.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import textShadowGenerator from "../Utilities/TextEffects";
 import * as PropTypes from "../Utilities/PropTypes";
 import BSCode from "../../public/static/images/BSCode.png";
+import PersonalWebsiteCode from "../../public/static/images/PersonalWebsiteCode.png";
+import CNNCatDogCode from "../../public/static/images/CNNCatDogCode.png";
 
 export default function PortfolioBody({
 	headerShadowColor,
@@ -20,7 +22,27 @@ export default function PortfolioBody({
 			</h1>
 			<div id="portfolioGrid">
 				<div className="portfolioGridItem">
-					<h2 className="portfolioGridProjectName">Project1</h2>
+					<Link href="https://github.com/hayden4r4/personal-website-frontend">
+						<div
+							className="portfolioGridItemBody"
+							title="Github/hayden4r4/personal-website-frontend"
+						>
+							<h2 className="portfolioGridProjectName">Personal Website Frontend</h2>
+
+							<h4 className="portfolioGridLangDate">
+								TS HTML SCSS 🕸 Rust 🦀 | 2022
+							</h4>
+							<p className="portfolioGridProjectDescription">
+								My personal website built with Next.js, SCSS, and Rust WASM
+							</p>
+							<Image
+								src={PersonalWebsiteCode}
+								alt="Personal Website Code Screenshot"
+								quality={100}
+								placeholder="blur"
+							></Image>
+						</div>
+					</Link>
 				</div>
 				<div className="portfolioGridItem">
 					<Link href="https://github.com/hayden4r4/blackscholes-rust">
@@ -35,17 +57,63 @@ export default function PortfolioBody({
 							<h4 className="portfolioGridLangDate">Rust 🦀 | 2022</h4>
 							<p className="portfolioGridProjectDescription">
 								An implementation of the Black-Scholes option pricing model in
-								Rust.
+								Rust
 							</p>
 							<Image
 								src={BSCode}
 								alt="Black-Scholes Calculator Code Screenshot"
+								quality={100}
+								placeholder="blur"
 							></Image>
 						</div>
 					</Link>
 				</div>
 				<div className="portfolioGridItem">
-					<h2 className="portfolioGridProjectName">Project3</h2>
+					<Link href="https://github.com/hayden4r4/CNN_Cat_Dog">
+						<div
+							className="portfolioGridItemBody"
+							title="Github/hayden4r4/CNN_Cat_Dog"
+						>
+							<h2 className="portfolioGridProjectName">
+								TensorFlow CNN Cat Dog Model
+							</h2>
+
+							<h4 className="portfolioGridLangDate">Python 🐍 | 2022</h4>
+							<p className="portfolioGridProjectDescription">
+								A model trained with a CNN in TensorFlow to classify images of
+								cats and dogs
+							</p>
+							<Image
+								src={CNNCatDogCode}
+								alt="CNN Cat Dog Code Screenshot"
+								quality={100}
+								placeholder="blur"
+							></Image>
+						</div>
+					</Link>
+				</div>
+				<div className="portfolioGridItem">
+					<Link href="https://github.com/hayden4r4/GEX-python">
+						<div
+							className="portfolioGridItemBody"
+							title="Github/hayden4r4/GEX-python"
+						>
+							<h2 className="portfolioGridProjectName">
+								Gamma Exposure (GEX)
+							</h2>
+
+							<h4 className="portfolioGridLangDate">Python 🐍 | 2022</h4>
+							<p className="portfolioGridProjectDescription">
+								A python script to calculate the Gamma Exposure for a given stock/future/index using TDAmeritrade for data
+							</p>
+							<Image
+								src={CNNCatDogCode}
+								alt="CNN Cat Dog Code Screenshot"
+								quality={100}
+								placeholder="blur"
+							></Image>
+						</div>
+					</Link>
 				</div>
 			</div>
 		</div>
