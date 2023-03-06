f72b5b5464d2b25ef3d176f414ef0e68bbdc0f65
Hayden Rose <hayden4r4@gmail.com>
2023-03-05 18:48:13 -0800

    made the greeks output in the option pricer into grid

--- pages/blackscholes.tsx
+++ pages/blackscholes.tsx
@@ -13,7 +13,7 @@ export default function BlackScholes() {
 		<div id="blackScholesApp">
 			<Head>
 				<title>Portfolio - Hayden Rose</title>
-				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<meta name="viewport" content="initial-scale=1.0, width=device-width user-scalable=no"/>
 				<meta name="darkreader-lock" />
 				<meta name="color-scheme" content="light" />
 			</Head>
